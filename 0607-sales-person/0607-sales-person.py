26c81e0cec5a1fb629b0867a08d0828074aa9d4d
Dhruv Yadav <dhruvyadav612@gmail.com>
2025-04-08 17:16:43 +0530

    Time: 382 ms (86.87%), Space: 68.1 MB (51.3%) - LeetHub

--- 0607-sales-person/0607-sales-person.py
+++ 0607-sales-person/0607-sales-person.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+# Took Help from the solution I forgot to use isin() and ~ earlier
 def sales_person(sales_person: pd.DataFrame, company: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
     return sales_person[
         ~sales_person['sales_id'].isin(
