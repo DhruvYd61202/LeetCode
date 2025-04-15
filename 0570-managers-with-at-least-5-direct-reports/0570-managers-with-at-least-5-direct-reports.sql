01fdba8c6f90f8d87efc986eab2592e40075875d
Dhruv Yadav <dhruvyadav612@gmail.com>
2025-04-15 13:11:45 +0530

    Time: 354 ms (80.64%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,18 @@
-select name from employee where id in
-(
-select managerId
-from employee
-group by managerId
-having count(*) >= 5
+#using the IN operator
+-- select name from employee where id in
+-- (
+-- select managerId
+-- from employee
+-- group by managerId
+-- having count(*) >= 5
 
-)
+-- )
+
+#Can Also be done using join function
+#Need to understand this how this is working?
+
+#Using the join operator
+SELECT Name FROM Employee WHERE Id IN 
+
+(SELECT ManagerId from employee GROUP BY ManagerId
+HAVING (COUNT(DISTINCT Id) >=5 ))
