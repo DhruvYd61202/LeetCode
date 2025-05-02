3d2f39a841bf753e29c6e47adb30b0bfdd065d8a
Dhruv Yadav <dhruvyadav612@gmail.com>
2025-05-02 10:50:37 +0530

    Time: 8 ms (77.96%), Space: 18 MB (50.62%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,21 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        count = 0
+        result = [[] for _ in range(numRows)]
+        flag = True
+        for i in range(len(s)):
+            if count < numRows:
+                result[count].append(s[i])
+                if flag:
+                    count += 1
+                else:
+                    count -= 1
+            if count == numRows:
+                count -= 2
+                flag = False
+            if count == 0:
+                flag = True
+        return ''.join([i for j in result for i in j])
+# print(convert("ADITYASINGH", 3))
