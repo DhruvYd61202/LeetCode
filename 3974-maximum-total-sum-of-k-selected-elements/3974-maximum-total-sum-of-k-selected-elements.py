d62ea00bf18f96d1697f1fc2e437b3274b61d1b7
Dhruv Yadav <dhruvyadav612@gmail.com>
2026-07-04 16:08:12 +0530

    Time: 168 ms (93.22%), Space: 34.3 MB (82.99%) - LeetHub

---
+++ 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxSum(self, nums: list[int], k: int, mul: int) -> int:
+        nums.sort()
+
+        # Base sum of largest k elements
+        ans = sum(nums[-k:])
+
+        t = min(k, mul - 1)
+
+        # Extra gain from multiplication
+        for j in range(t):
+            ans += nums[-1 - j] * (mul - 1 - j)
+
+        return ans
